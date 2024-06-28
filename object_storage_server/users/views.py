66ad2f3a33538b48ccd205bd718f64fdb78d8279
Arshia Shafiei <arshiashafiei03@gmail.com>
2024-06-28 19:04:18 +0330

    Users can register now

--- object_storage_server/users/views.py
+++ object_storage_server/users/views.py
@@ -6,11 +6,15 @@ from .forms import UserRegisterForm
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
+        print(form.is_valid())
+        print(form.error_messages)
+        print(form.errors)
+        print(form.fields)
         if form.is_valid():
             form.save()
             messages.success(request,
                              'Your account has been registred, Now you can log in!')
-            return redirect('login')
+            return render("success")
     else:
         form = UserRegisterForm()
     context = {
