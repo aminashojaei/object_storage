59dcf1d8e0f64407ab9445a6706ae3d0ace2d0bc
Arshia Shafiei <arshiashafiei03@gmail.com>
2024-07-01 04:48:40 +0330

    add email auth

--- object_storage_server/object_storage_server/urls.py
+++ object_storage_server/object_storage_server/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', user_views.register, name='register'),
+    path('activate/<uidb64>/<token>/', user_views.activate, name='activate'),
+    path('account_activation_sent/', user_views.account_activation_sent, name='account_activation_sent'),
     path('', include('storage.urls'))
 ]
--- object_storage_server/users/views.py
+++ object_storage_server/users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.core.mail import send_mail
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.auth.tokens import default_token_generator
+from django.conf import settings
+from django.contrib.auth.models import User
 from .forms import UserRegisterForm
 
 
@@ -7,10 +12,23 @@ def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request,
-                             'Your account has been registred, Now you can log in!')
-            return redirect("login")
+            user = form.save(commit=False)
+            user.is_active = False  # Deactivate account till it is confirmed
+            user.save()
+
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            verification_link = request.build_absolute_uri(f'/activate/{uid}/{token}/')
+
+            send_mail(
+                'Account Activation',
+                f'Click the link to activate your account: {verification_link}',
+                settings.EMAIL_HOST_USER,
+                [user.email],
+                fail_silently=False,
+            )
+
+            return redirect('account_activation_sent')
     else:
         form = UserRegisterForm()
     context = {
@@ -18,3 +36,22 @@ def register(request):
         'title': 'Registration'
     }
     return render(request, 'users/register.html', context)
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect('login')
+    else:
+        return render(request, 'verify_account_invalid.html')
+
+
+def account_activation_sent(request):
+    return render(request, 'verify_account_sent.html')
